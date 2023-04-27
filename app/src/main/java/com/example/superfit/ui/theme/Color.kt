1ed93e1aa9a1a0fc195a433bd7afda71f832ffa4
Denis5201 <builukden@yandex.ru>
2023-04-27 21:09:30 +0700

    Init

---
+++ app/src/main/java/com/example/superfit/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.superfit.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Background = Color(0xFF282828)
+val BackgroundLight = Color(0xFF424242)
+val Purple = Color(0xFFB461F5)
+val PurpleLight = Color(0xFFBB86FC)
+val PurpleDark = Color(0xFFBB6BD9)
+val Gray = Color(0xFFC6C6C6)
+val GrayLight = Color(0xFFF2F2F2)
+val White48 = Color(0x7AFFFFFF)
+val White70 = Color(0xB3FFFFFF)
+
+val SystemStatusBarColor = Color(0x33000000)
